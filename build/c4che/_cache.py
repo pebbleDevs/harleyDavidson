af91348bb380f9375ad5edc411c5791c6ed8456f
Alok Choudhary <alok.c83@gmail.com>
2015-08-31 20:51:26 -0400

    Operation : Carnegie

--- build/c4che/_cache.py
+++ build/c4che/_cache.py
@@ -1,6 +1,6 @@
 BINDIR = '/usr/local/bin'
 DEFINES = ['RELEASE']
 LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/alokchoudhary/pebble-dev/PebbleSDK-3.0-dp7/Pebble/common'
+PEBBLE_SDK_COMMON = '/Users/alokchoudhary/pebble-dev/PebbleSDK-3.0-dp8/Pebble/common'
 PREFIX = '/usr/local'
 TARGET_PLATFORMS = [u'aplite']
